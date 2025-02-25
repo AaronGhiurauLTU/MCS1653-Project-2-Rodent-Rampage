f8e27d1eaa19289e0cf34b24484ebf6625861689
aaronghiurau <samghiu@gmail.com>
2025-02-25 11:15:08 -0500

    Tiles highlight under mouse, fixed camera scaling

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -3,26 +3,62 @@ using System;
 
 public partial class GameManager : Node2D
 {
-	TileMapLayer tileMap;
+	[Export] private TileMapLayer tileMap;
+
+	// null indicates no tile is highlighted, store the tilemap coordinates of the tile the mouse is over
+	private Vector2I? currentTileCoordinates;
+
+	// store the atlas coordinates of the current tile that the mouse is over
+	private Vector2I? currentTileAtlasCoordinates;
+
+	private string placeableTiles = "Background";
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		tileMap = GetNode<TileMapLayer>("%Background");
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		// offset mouse position by half the tile size for accurate detection
-		Vector2 mousePos = GetViewport().GetMousePosition() + Vector2.Down * 20;
+		Vector2 mousePos = GetViewport().GetMousePosition() + (Vector2.Down * tileMap.TileSet.TileSize.X / 2);
 
 		Vector2I tilePos = tileMap.LocalToMap(tileMap.ToLocal(mousePos));
 
 		TileData tileData = tileMap.GetCellTileData(tilePos);
-
-		if (tileData is not null)
+		
+		// check the name of the tile to see if it is the current placeable tiles name or is highlighted
+		if ((string)tileData?.GetCustomData("Name") == placeableTiles || (string)tileData?.GetCustomData("Name") == "Highlight")
 		{
-			GD.Print(tileData.GetCustomData("Name"));
+			// no need to update anything if the mouse stayed over the same tile last frame
+			if (currentTileCoordinates != tilePos)
+			{
+				ResetPreviousTile();
+
+				// update the current tile information
+				currentTileCoordinates = tilePos;
+				currentTileAtlasCoordinates = tileMap.GetCellAtlasCoords(tilePos);
+
+				// set cell to the highlighted cell
+				tileMap.SetCell(tilePos, 1,  new Vector2I(0, 0));
+			}
 		} 
+		else
+		{
+			ResetPreviousTile();
+			currentTileCoordinates = null;
+			currentTileAtlasCoordinates = null;
+		}
+	}
+
+	private void ResetPreviousTile()
+	{
+		// set the previously highlighted tile back to what it was originally
+		if (currentTileCoordinates != null)
+		{
+			tileMap.SetCell((Vector2I)currentTileCoordinates, 1,  currentTileAtlasCoordinates);
+		}
 	}
 }
