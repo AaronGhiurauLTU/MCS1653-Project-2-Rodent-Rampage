15d83799ac41c1c9e90b3c5a86aa76f813a4a5ef
aaronghiurau <samghiu@gmail.com>
2025-03-23 20:00:54 -0400

    New+tweaked laptops and visual tweaks

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Menu : Control
 {
+	[Export] private MarginContainer instructionsContainer, mainMenuContainer;
+	[Export] private GameManager gameManager;
 	private void OnPlayPressed()
 	{
 		Engine.TimeScale = 1;
@@ -12,11 +14,30 @@ public partial class Menu : Control
 	private void OnMainMenuPressed()
 	{
 		Engine.TimeScale = 1;
+		GD.Print("main menu");
 		GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
 	}
 
 	private void OnQuitPressed()
 	{
 		GetTree().Quit();
+	}
+
+	private void OnInstructionsPressed()
+	{
+		instructionsContainer.Visible = true;
+		mainMenuContainer.Visible = false;	
+	}
+	private void OnInstructionsBackPressed()
+	{
+		instructionsContainer.Visible = false;
+		mainMenuContainer.Visible = true;
+	}
+
+	private void OnContinuePressed()
+	{
+		Visible = false;
+		Engine.TimeScale = 1;
+		MusicManager.SetBusVolume("Music", 0);
 	}
 }
